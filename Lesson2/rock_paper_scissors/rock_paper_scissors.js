a6279491daaf0acd0ec79e903eaa22152c744116
Stefano Schmidt <schmidt.stefano@outlook.com>
2020-06-23 15:53:06 -0400

    Added comments with Things to Consider at the end of rock_paper_scissors.js file

--- Lesson2/rock_paper_scissors/rock_paper_scissors.js
+++ Lesson2/rock_paper_scissors/rock_paper_scissors.js
@@ -47,4 +47,31 @@ while(true) {
 
 }
 
+/* 
+THINGS TO CONSIDER
+1) Functions defined with the function keyword are hoistered, so moving the
+displayWinner() function definition aboove the prompt() definition does not
+change how anything in this program works.
+2) If we chose/defined displayWinner to return a string instead of calling the
+prompt() function directly, we could achieve the same functionality by using 
+the value returned by displayWinner() as argument to prompt() through function
+composition i.e. calling prompt(displayWinner(choice, computerChoice)).
+3) We could rewrite the line that picks randomIndex as 
+- let randomIndex = Math.round(Math.random*(VALID_CHOICES.length - 1)) or as
+- let randomIndex = Math.ceil(Math.random*(VALID_CHOICES.length - 1) - 1).
+4) We can rewrite the while loop with the always true condition as:
+let answer = 'y';
+while (answer[0] === 'y') {
+  // same code 
+
+  prompt('Do you want to play again (y/n)?');
+  answer = readline.question().toLowerCase();
+  while(answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "n" or "y"');
+    answer = readline.question().toLowerCase();
+  }
+
+  // remove break condition
+}
+*/
 
